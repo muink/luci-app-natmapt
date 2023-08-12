d025621789cca699c450df4256224bf4303c6af8
Anya Lin <hukk1996@gmail.com>
2023-08-12 23:16:36 +0800

    luci-app-natmap: Allows manual reload
    Signed-off-by: Anya Lin <hukk1996@gmail.com>

--- applications/luci-app-natmap/htdocs/luci-static/resources/view/natmap.js
+++ applications/luci-app-natmap/htdocs/luci-static/resources/view/natmap.js
@@ -80,6 +80,18 @@ return view.extend({
 		s = m.section(form.TypedSection, 'global');
 		s.anonymous = true;
 
+		o = s.option(form.Button, '_reload', _('Reload'));
+		o.inputtitle = _('Reload');
+		o.inputstyle = 'apply';
+		o.onclick = function() {
+			window.setTimeout(function() {
+				window.location = window.location.href.split('#')[0];
+			}, L.env.apply_display * 500);
+
+			return fs.exec('/etc/init.d/natmap', ['reload'])
+				.catch(function(e) { ui.addNotification(null, E('p', e.message), 'error') });
+		};
+
 		o = s.option(form.Flag, 'enable', _('Enable'));
 		o.default = o.disabled;
 		o.rmempty = false;
